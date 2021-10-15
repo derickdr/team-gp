5ba44e88a1ece1e3f84693c03a782ef3c2ba23e6
Derick Del Rosario <derickmitchel@icloud.com>
2021-10-15 09:57:29 -0700

    added splash

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import Splash from './Splash';
 import About from './About';
 
 class HomePage extends Component {
     render() {
         return (
+            <div className='home-container'>
+            <Splash />
             <About />
+            </div>
         );
     };
 };
